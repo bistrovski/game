31235efc63bac10e2f757e7ef948f299a1264d48
Taras <vitoolichka@gmail.com>
2020-11-11 22:20:25 +0100

    add robot

---
+++ Assets/Scripts/followCamera.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using UnityEngine;
+
+public class followCamera : MonoBehaviour
+{
+    public GameObject target;
+    Vector3 offset;
+    void Start()
+    {
+        offset =  new Vector3(0, 2, -6);
+    }
+
+
+    void Update()
+    {
+        transform.LookAt(target.transform);
+        transform.position = target.transform.position + offset;
+    }
+}
---
+++ Assets/Scripts/playerController.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerController : MonoBehaviour
+{
+    public Animator anim;
+    public CharacterController controller;
+    public float speed = 2;  
+    public float turnSpeed = 1000;
+    public float gravity = -12;
+	public float jumpHeight = 1;
+    float velocityY;
+    float velocityX;
+    void Start()
+    {
+        
+      
+    }
+
+    void Update()
+    {
+        velocityY += Time.deltaTime * gravity; // gravity   
+        
+        velocityX = Input.GetAxis("Horizontal") * Time.deltaTime * turnSpeed; // turn right or left
+        
+        Vector3 velocity  = Vector3.right * velocityX + transform.forward * speed + Vector3.up * velocityY; // turn right or left + run forward + gravity
+        
+        
+        controller.Move (velocity * Time.deltaTime); 
+
+        if (controller.isGrounded) {
+			velocityY = 0;
+		}
+
+        if(Input.GetButtonDown("Jump")) // jump 
+        {            
+            Jump();
+            
+        }
+    }
+
+    void Jump() {
+        
+		if (controller.isGrounded) {
+            anim.SetTrigger("jump");
+			float jumpVelocity = Mathf.Sqrt (-2 * gravity * jumpHeight);
+			velocityY = jumpVelocity;
+		}
+    }
+}
