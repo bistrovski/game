f14c9f862018f5e9265f71b44fb0bbcba1f19b28
Nurken <nurquen@mail.ru>
2021-01-15 03:06:20 +0500

    MainMenu with Exit

---
+++ Assets/Scenes/ControlSceneAndMenu.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControlSceneAndMenu : MonoBehaviour
+{
+    public GameObject buttonsMenu;
+    public GameObject buttonsExit;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ShowExitButtons(){
+        buttonsMenu.SetActive(false);
+        buttonsExit.SetActive(true);
+    }
+    public void BackInMenu()
+    {
+        buttonsMenu.SetActive(true);
+        buttonsExit.SetActive(false);
+    }
+    public void ExitGame(){
+        Application.Quit();
+    }
+
+    public void NewGameLoadSceneSampleScene(){
+        Application.LoadLevel("SampleScene");
+    }
+}
